688f29dd27a2f373cfa2f15b0bd448d405598d93
dylan-clarry <dylan.imail@gmail.com>
2020-03-10 20:43:35 -0400

    fixed naming issue

--- exporter.py
+++ exporter.py
@@ -10,22 +10,16 @@ def prep_tsv():
 
 
 	deck = episode.get_episode_suffix()
-	sound_name = deck + "_" + sub.get_start() + "-" + sub.get_end() + ".mp3"
-	img_name = deck + "_" + sub.get_start() + "-" + sub.get_end() + ".jpg"
+	sound_name = deck + "_" + sub.get_start().replace(':', '-') + "-" + sub.get_end().replace(':', '-') + ".mp3"
+	img_name = deck + "_" + sub.get_start().replace(':', '-') + "-" + sub.get_end().replace(':', '-') + ".jpg"
 
 	sound = "[sound:" + sound_name + "]"
 	img = "<img src=\"" + img_name + "\">"
 	content = sub.get_content()
 
-	print("tsv card")
-	print("==============")
-	print("deck: ", deck)
-	print("sound: ", sound_name)
-	print("img: ", img_name)
-	print("content: ", content)
 
 	# export media
-	media_dir = episode.get_deck_dir() + '/media/'
+	media_dir = episode.get_deck_dir() + 'media/'
 
 	sp.call(['ffmpeg', '-i', episode.get_audio_file(), '-ss', sub.get_start(), '-t', sub.get_dur(), '-q:a', '0', '-map', 'a', media_dir + '' + sound_name])
 	sp.call(['ffmpeg', '-ss', sub.get_end(), '-i', episode.get_episode_file(), '-vframes', '1', '-q:v', '2', media_dir + '' + img_name])
@@ -35,3 +29,11 @@ def prep_tsv():
 	with open(deck_dir, 'wt') as out_file:
 		tsv_writer = csv.writer(out_file, delimiter='\t')
 		tsv_writer.writerow([deck, sound, img, content])
+	
+
+	print("tsv card")
+	print("==============")
+	print("deck: ", deck)
+	print("sound_name: ", media_dir + '' + sound_name, type(media_dir + '' + sound_name))
+	print("img_name: ", media_dir + '' + img_name, type(media_dir + '' + img_name))
+	print("content: ", content)
